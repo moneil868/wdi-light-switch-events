3293345adce1e6db39c22020ec9525504d88fc52
Marlon O'Neil <hello@marlononeil.com>
2017-10-03 14:34:17 -0400

    Completed exercise

--- js/main.js
+++ js/main.js
@@ -1,2 +1,21 @@
+document.addEventListener('DOMContentLoaded', function () {
+
+  var lightSwitch = document.querySelector('button');
+  var bodyClass = document.querySelector('body');
+  var message = document.querySelector('h1');
 
 
+  lightSwitch.addEventListener('click', function() {
+    if(lightSwitch.classList == 'switch on'){
+      lightSwitch.classList = 'switch off';
+      bodyClass.classList = 'dark';
+      message.innerText = 'Hey, who turned off the lights?';
+
+    }
+    else {
+      lightSwitch.classList = 'switch on';
+      bodyClass.classList = 'light';
+      message.innerText = "It's so bright in here!";
+    }
+  });
+})
